d07dc8f60d63f37869405c837ed89ee86a294770
jamesfeng2 <jamesfeng2@users.noreply.github.com>
2020-12-28 15:08:39 +1100

    Update index.php

--- index.php
+++ index.php
@@ -106,6 +106,7 @@ else {
         echo "<p>Sorry, there was an error uploading your file.</p>";
     }
 }
+    echo "<div> <h1> do more file"
 echo "</div>";
 }
 echo "<h3>Uploaded Files</h3>";
@@ -115,6 +116,6 @@ foreach ($files as $f) {
 }
 ?>
 </div>
-<div> <h1> do more file
+
 </body>
 </html>
